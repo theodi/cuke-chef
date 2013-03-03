3e712584468a235887475eee938781be1eeb69da
pikesley <github@orgraphone.org>
2013-03-03 22:05:20 +0000

    This seems to work correctly now

--- site-cookbooks/odi-deployment/recipes/default.rb
+++ site-cookbooks/odi-deployment/recipes/default.rb
@@ -64,19 +64,23 @@ end
       dbi = data_bag_item("%s" % [
         node['git_project']
       ], f.split('.').first)
-#      dbi = data_bag_item('member-directory', f.split('.').first)
+      puts dbi
 
-    content dbi["content"].to_yaml
+      content dbi["content"].to_yaml
+
     rescue Net::HTTPServerException
     end
+
   end
 end
 
-deploy_revision "memberstheodiorg" do
+deploy_revision node['git_project'] do
   deploy_to root_dir
   user node['user']
   group node['group']
-  repo 'git://github.com/theodi/member-directory.git'
+  repo "git://github.com/theodi/%s.git" % [
+    node['git_project']
+  ]
 #  revision 'feature-production-deploy'
   symlink_before_migrate(
     {
@@ -85,9 +89,6 @@ deploy_revision "memberstheodiorg" do
     }
   )
   before_migrate do
-#    execute "bundle" do
-#      command "su - members -c 'cd #{root_dir}/current && bundle'"
-#    end
     current_release_directory = release_path
     running_deploy_user = new_resource.user
     bundler_depot = new_resource.shared_path + '/bundle'
@@ -97,10 +98,10 @@ deploy_revision "memberstheodiorg" do
       interpreter 'bash'
       cwd current_release_directory
       user running_deploy_user
-      code <<-EOS
-    bundle install --quiet --deployment --path #{bundler_depot} \
-      --without #{excluded_groups.join(' ')}
-      EOS
+      code <<-EOF
+        bundle install --quiet --deployment --path #{bundler_depot} \
+          --without #{excluded_groups.join(' ')}
+      EOF
     end
   end
 #  migrate true
