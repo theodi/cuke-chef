cade93c3411b1f53b16df3c83ed9081e07bd9fbf
pikesley <github@orgraphone.org>
2013-03-07 14:35:02 +0000

    There was a lot of fighting here
    Still not entirely happy with this, but we do have a full set of tests,
    so I'll fix it as I go

--- site-cookbooks/odi-deployment/recipes/default.rb
+++ site-cookbooks/odi-deployment/recipes/default.rb
@@ -26,21 +26,25 @@
 
 include_recipe 'git'
 
+if node.chef_environment == 'production'
+
+
+
 root_dir = "/var/www/%s" % [
-  node['project_fqdn']
+    node['project_fqdn']
 ]
 
 [
-  "",
-  "shared",
-  "shared/config",
-  "shared/pids",
-  "shared/log",
-  "shared/system"
+    "",
+    "shared",
+    "shared/config",
+    "shared/pids",
+    "shared/log",
+    "shared/system"
 ].each do |d|
   directory "%s/%s" % [
-    root_dir,
-    d
+      root_dir,
+      d
   ] do
     owner node['user']
     group node['group']
@@ -50,67 +54,110 @@ root_dir = "/var/www/%s" % [
 end
 
 [
-  "database.yml",
-  "env"
+    "database.yml",
+    "env"
 ].each do |f|
-  p = "%s/%s/%s" % [
-    root_dir,
-    "shared/config",
-    f
+  p   = "%s/%s/%s" % [
+      root_dir,
+      "shared/config",
+      f
   ]
+  dbi = nil
   file p do
     action :create
     begin
       dbi = data_bag_item("%s" % [
-        node['git_project']
-      ], f.split('.').first)
-      puts dbi
+          node['git_project']
+      ],                  f.split('.').first)
 
       content dbi["content"].to_yaml
 
     rescue Net::HTTPServerException
     end
-
   end
+
 end
 
-deploy_revision node['git_project'] do
-  deploy_to root_dir
+deploy_revision root_dir do
   user node['user']
   group node['group']
+  environment "RACK_ENV" => node['RACK_ENV'] #,
+#              "rvmsudo_secure_path" => 1
+
   repo "git://github.com/theodi/%s.git" % [
-    node['git_project']
+      node['git_project']
   ]
-#  revision 'feature-production-deploy'
+
   symlink_before_migrate(
-    {
-      "config/database.yml" => "config/database.yml",
-      "config/env" => ".env"
-    }
+      {
+          "config/database.yml" => "config/database.yml",
+          "config/env"          => ".env"
+      }
   )
+
   before_migrate do
     current_release_directory = release_path
-    running_deploy_user = new_resource.user
-    bundler_depot = new_resource.shared_path + '/bundle'
-    excluded_groups = %w(development test)
+    running_deploy_user       = new_resource.user
+    bundler_depot             = new_resource.shared_path + '/bundle'
 
     script 'Bundling the gems' do
       interpreter 'bash'
       cwd current_release_directory
       user running_deploy_user
       code <<-EOF
-        bundle install --quiet --deployment --path #{bundler_depot} \
-          --without #{excluded_groups.join(' ')}
+        bundle install \
+          --quiet \
+          --path #{bundler_depot}
       EOF
     end
   end
-#  migrate true
-# RAILS_ENV=production rake db:migrate ???
-# rake assets:precompile ???
-# also RAILS_ENV issues, also had to set
-#      config.serve_static_assets = true
-# nginx should really be doing this
-#  before_restart "rvmsudo foreman export upstart /etc/init"
-#  restart_command "service member-directory restart"
+
+  migration_command "bundle exec rake db:migrate"
+  migrate true
+
+  before_restart do
+    current_release_directory = release_path
+    running_deploy_user       = new_resource.user
+
+    script 'Precompile the assets' do
+      interpreter 'bash'
+      cwd current_release_directory
+      user running_deploy_user
+      code <<-EOF
+        bundle exec rake assets:precompile
+      EOF
+    end
+
+# THIS IS FUCKED
+    script 'Generate startup scripts with Foreman' do
+      interpreter 'bash'
+      cwd current_release_directory
+      user running_deploy_user
+      code <<-EOF
+        export rvmsudo_secure_path=1
+        /home/#{user}/.rvm/bin/rvmsudo bundle exec foreman export \
+          -a #{node['git_project']} \
+          -u #{node['user']} \
+          -t config/foreman \
+          -c thin=1 \
+          -p 3000 \
+          upstart /etc/init
+      EOF
+    end
+  end
+
+  restart_command "sudo service #{node['git_project']} restart"
   action :force_deploy
+end
+
+
+## rake assets:precompile ???
+## also RAILS_ENV issues, also had to set
+##      config.serve_static_assets = true
+## nginx should really be doing this
+##  before_restart "rvmsudo foreman export upstart /etc/init"
+##  restart_command "service member-directory restart"
+#  action :force_deploy
+#end
+
 end
