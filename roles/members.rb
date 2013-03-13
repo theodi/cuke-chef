52a6cdfb88465d12cc35da33da04282606faeab8
pikesley <github@orgraphone.org>
2013-03-13 16:57:53 +0000

    Lots more stuff

--- roles/members.rb
+++ roles/members.rb
@@ -1,17 +1,32 @@
 name 'members'
 
-default_attributes 'user' => 'members',
-                   'group' => 'members',
-                   'ruby' => '1.9.3-p374',
-                   'project_fqdn' => 'members.theodi.org',
-                   'git_project' => 'member-directory'
+default_attributes 'user'              => 'members',
+                   'group'             => 'members',
+                   'ruby'              => '1.9.3-p374',
+                   'project_fqdn'      => 'members.theodi.org',
+                   'git_project'       => 'member-directory',
+                   'migration_command' => 'bundle exec rake db:migrate:with_data'
+
+override_attributes 'envbuilder'  => {
+    'base_dir' => '/var/www/members.theodi.org/shared/config',
+    'owner'    => 'members',
+    'group'    => 'members'
+},
+                    'chef_client' => {
+                        'interval' => 600
+                    }
+
 
 run_list 'apt',
+         'chef-client',
          'build-essential',
          'git',
+         'imagemagick',
+         'nginx',
          'odi-users',
          'odi-rvm',
          'odi-xml',
+         'odi-nginx',
          'xslt',
          'libcurl',
          'nodejs',
@@ -19,5 +34,5 @@ run_list 'apt',
          'sqlite::dev',
          'redisio::install',
          'redisio::enable',
-         'nginx',
+         'envbuilder',
          'odi-deployment'
