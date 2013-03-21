4bee170fed7e4581cb28c6e91b1bb5a5252262c5
pikesley <github@orgraphone.org>
2013-03-21 15:53:36 +0000

    We now include the base role

--- roles/directory.rb
+++ roles/directory.rb
@@ -13,30 +13,25 @@ override_attributes 'envbuilder'  => {
     'group'    => 'directory'
 },
                     'chef_client' => {
-                        'interval' => 1800,
-                        'splay'    => 300
                         'interval' => 300,
                          'splay'     => 30
                     }
 
 
-run_list 'apt',
-         'chef-client',
-         'chef-client::config',
-         'build-essential',
-         'git',
-         'imagemagick',
-         'nginx',
-         'odi-users',
-         'odi-rvm',
-         'odi-xml',
-         'odi-nginx',
-         'xslt',
-         'libcurl',
-         'nodejs',
-         'mysql::client',
-         'sqlite::dev',
-         'redisio::install',
-         'redisio::enable',
-         'envbuilder',
-         'odi-deployment'
+run_list "role[base]",
+         "recipe[build-essential]",
+         "recipe[imagemagick]",
+         "recipe[nginx]",
+         "recipe[odi-users]",
+         "recipe[odi-rvm]",
+         "recipe[odi-xml]",
+         "recipe[odi-nginx]",
+         "recipe[xslt]",
+         "recipe[libcurl]",
+         "recipe[nodejs]",
+         "recipe[mysql::client]",
+         "recipe[sqlite::dev]",
+         "recipe[redisio::install]",
+         "recipe[redisio::enable]",
+         "recipe[envbuilder]",
+         "recipe[odi-deployment]"
