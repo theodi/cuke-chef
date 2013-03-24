6c90b3bba43b6b028470fda0e265970fd7deeff4
pikesley <github@orgraphone.org>
2013-03-24 22:21:49 +0000

    Webserver role

---
+++ roles/odi-drupal.rb
@@ -0,0 +1,19 @@
+name 'odi-drupal'
+
+# default_attributes
+
+override_attributes 'chef_client' => {
+    'interval' => 0,
+    'splay'    => 30
+}
+
+run_list "role[base]",
+         "recipe[apache2::mod_php5]",
+         "recipe[apache2::mod_rewrite]",
+         "recipe[php::module_gd]",
+         "recipe[php::module_mysql]",
+         "recipe[odi-php-memcached]",
+         "recipe[git]",
+         "recipe[postfix]",
+         "recipe[drush]",
+         "recipe[odi-website-deploy]"
