6e5d0bc583a2b66bb52bace8cd7998013ee2f1ad
pikesley <github@orgraphone.org>
2013-03-25 15:21:51 +0000

    Wrapper cookbook
    Check if we're running on cuke-chef, and if so don't lay down the
    chef-client config

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,7 @@
 name 'base'
 
 run_list "recipe[apt]",
-         "recipe[chef-client::config]",
+         "recipe[odi-chef-client::config]",
          "recipe[git]",
-         "recipe[postfix]"         "recipe[ntp]"
+         "recipe[postfix]",
+         "recipe[ntp]"
