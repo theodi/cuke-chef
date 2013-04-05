7921e572acb415acd5f9c3c15f43a4518b26ea00
pezholio <pezholio@gmail.com>
2013-04-05 11:24:51 +0100

    First pass of the feature to get Office Calendar into production

---
+++ roles/calendar.rb
@@ -0,0 +1,44 @@
+name 'calendar'
+
+default_attributes 'user'              => 'calendar',
+                   'group'             => 'calendar',
+                   'ruby'              => '1.9.3-p374',
+                   'project_fqdn'      => 'calendar.theodi.org',
+                   'git_project'       => 'office-calendar',
+                   'migration_command' => 'bundle exec rake db:migrate',
+                   'chef_client'       => {
+                       'cron' => {
+                           'use_cron_d' => true,
+                           'hour'       => "*",
+                           'minute'     => "*/5",
+                           'log_file'   => "/var/log/chef/cron.log"
+                       }
+                   }
+
+override_attributes 'envbuilder'  => {
+    'base_dir' => '/var/www/calendar.theodi.org/shared/config',
+    'owner'    => 'calendar',
+    'group'    => 'calendar'
+},
+                    'chef_client' => {
+                        'interval' => 300,
+                        'splay'    => 30
+                    }
+
+
+run_list "role[base]",
+         "recipe[chef-client::cron]",
+         "recipe[build-essential]",
+         "recipe[nginx]",
+         "recipe[odi-users]",
+         "recipe[odi-rvm]",
+         "recipe[odi-xml]",
+         "recipe[odi-nginx]",
+         "recipe[xslt]",
+         "recipe[libcurl]",
+         "recipe[nodejs::install_from_package]",
+         "recipe[mysql::client]",
+         "recipe[sqlite::dev]",
+         "recipe[envbuilder]",
+         "recipe[odi-deployment]",
+         "recipe[odi-shim]"
