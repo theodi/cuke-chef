78be67ff72d86fbb7c784bd97f111bd35e050d01
pezholio <pezholio@gmail.com>
2013-04-11 15:28:19 +0100

    Get Drupal into Rackspace
    All the tests pass now

---
+++ site-cookbooks/odi-fileconveyor/metadata.rb
@@ -0,0 +1,7 @@
+name             'odi-fileconveyor'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs/Configures odi-fileconveyor'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
