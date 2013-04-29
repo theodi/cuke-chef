e807ffff555c4136e5f9ec1766a9b6848a2253e6
pikesley <github@orgraphone.org>
2013-04-29 20:46:55 +0100

    config

--- .cucumber-chef/config.rb
+++ .cucumber-chef/config.rb
@@ -1,7 +1,29 @@
-provider                        :vagrant
-librarian_chef                  true
-artifacts                       ({ "chef-client-log" => "/var/log/chef/client.log",
-                                   "chef-client-stacktrace" => "/var/chef/cache/chef-stacktrace.out" })
-vagrant.merge!(                 :identity_file => "#{ENV['HOME']}/.vagrant.d/insecure_private_key",
-                                :lab_user => "vagrant",
-                                :lxc_user => "root" )
+# If you want to customize configuration options do so here:
+
+provider        :vagrant
+
+vagrant.merge!( :identity_file => "#{ENV['HOME']}/.vagrant.d/insecure_private_key",
+                :ssh => {
+                    :lab_ip => "192.168.33.10",
+                    :lab_port => 22,
+                    :lxc_port => 22
+                },
+                :cpus => 4,
+                :memory => 4096 )
+
+# aws.merge!(     :identity_file => ENV['AWS_IDENTITY'],
+#                 :ssh => {
+#                     :lab_port => 22,
+#                     :lxc_port => 22
+#                 },
+#                 :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#                 :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+#                 :aws_ssh_key_id => ENV['AWS_SSH_KEY_ID'],
+#                 :region => "us-west-2",
+#                 :availability_zone => "us-west-2a",
+#                 :aws_instance_arch => "i386",
+#                 :aws_instance_type => "c1.medium" )
+
+artifacts       ({ "chef-client-log" => "/var/log/chef/client.log",
+                   "chef-client-stacktrace" => "/var/chef/cache/chef-stacktrace.out" })
+
