4d50b216e462a1c07349b68b3bc6e94e743fe5dc
pikesley <github@orgraphone.org>
2013-06-11 21:58:36 +0100

    for logstash

---
+++ roles/elasticsearch.rb
@@ -0,0 +1,19 @@
+name 'elasticsearch'
+
+default_attributes 'elasticsearch' => {
+    'cluster_name' => 'logstash',
+    'mlockall'     => false
+}
+
+override_attributes 'chef_client' => {
+    'cron' => {
+        'use_cron_d' => true,
+        'hour'       => "*",
+        'minute'     => "*/5",
+        'log_file'   => "/var/log/chef/cron.log"
+    }
+}
+
+run_list "role[base]",
+         "recipe[java]",
+         "recipe[elasticsearch]"
