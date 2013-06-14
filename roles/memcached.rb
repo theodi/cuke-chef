e6fbe43cf39c4762546edd8bb652796fe11e3aca
pikesley <github@orgraphone.org>
2013-06-14 13:44:58 +0100

    We like to Chef regularly

--- roles/memcached.rb
+++ roles/memcached.rb
@@ -1,4 +1,15 @@
 name 'memcached'
 
+override_attributes 'chef_client' => {
+    'cron' => {
+        'use_cron_d' => true,
+        'hour'       => "*",
+        'minute'     => "*/5",
+        'log_file'   => "/var/log/chef/cron.log"
+    }
+}
+
+
 run_list "role[base]",
+         "recipe[chef-client::cron]",
          "recipe[odi-memcached]"
