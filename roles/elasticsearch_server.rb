74ecd9d2e25881cdac2675657518fe79b5c4f61f
pikesley <github@orgraphone.org>
2013-06-19 16:44:42 +0100

    Play with chef-splay, use our wrapper cookbook

--- roles/elasticsearch_server.rb
+++ roles/elasticsearch_server.rb
@@ -1,19 +1,24 @@
 name 'elasticsearch_server'
 
+require 'ohai'
+
+
 default_attributes 'elasticsearch' => {
     'cluster_name' => 'logstash',
     'mlockall'     => false
 }
 
 override_attributes 'chef_client' => {
-    'cron' => {
+    'cron'  => {
         'use_cron_d' => true,
         'hour'       => "*",
         'minute'     => "*/5",
         'log_file'   => "/var/log/chef/cron.log"
-    }
+    },
+    'splay' => 250
 }
 
 run_list "role[base]",
+         "recipe[chef-client::cron]",
          "recipe[java]",
-         "recipe[elasticsearch]"
+         "recipe[odi-elasticsearch]"
