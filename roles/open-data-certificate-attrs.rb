e776deafd1dd33646c199f0c818fa10407a4c07a
pikesley <github@orgraphone.org>
2013-06-19 16:45:28 +0100

    Tune some things

--- roles/open-data-certificate-attrs.rb
+++ roles/open-data-certificate-attrs.rb
@@ -4,3 +4,10 @@ default_attributes "git_project"   => "open-data-certificate",
                    'odi-memcached' => {
                        'memory' => 768
                    }
+
+override_attributes "mysql" => {
+    "tunable" => {
+        "tmp_table_size" => "256M",
+        "query_cache_size" => "32M"
+    }
+}
