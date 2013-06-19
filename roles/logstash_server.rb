2d293e02b58cdf84c95312127625c9a67b6ef277
pikesley <github@orgraphone.org>
2013-06-19 16:44:57 +0100

    Use our wrapper book

--- roles/logstash_server.rb
+++ roles/logstash_server.rb
@@ -7,9 +7,22 @@ override_attributes 'chef_client' => {
         'minute'     => "*/5",
         'log_file'   => "/var/log/chef/cron.log"
     }
-}
+},
+                    'logstash'    => {
+                        'server'              => {
+                            'enable_embedded_es' => false
+                        },
+                        'elasticsearch_query' => nil,
+                        'elasticsearch_role'  => nil
+                    }
+
+#                    'logstash'    => {
+#                        'kibana' => {
+#                            'http_port' => 8080
+#                        }
+#                    }
 
 run_list 'role[base]',
-         'recipe[java]',
-         'recipe[logstash::server]',
-         'recipe[logstash::kibana]'
+         #         'recipe[java]',
+         'recipe[odi-logstash::server]'
+#         'recipe[logstash::kibana]'
