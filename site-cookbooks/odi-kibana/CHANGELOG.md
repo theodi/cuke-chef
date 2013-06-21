7dae94b6f13bf5185b261825f026d77edc75cbb5
pikesley <github@orgraphone.org>
2013-06-21 16:10:39 +0100

    Wrapper book
    I really must start filling in the READMEs

---
+++ site-cookbooks/odi-kibana/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for odi-kibana
+
+This file is used to list changes made in each version of odi-kibana.
+
+## 0.1.0:
+
+* Initial release of odi-kibana
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ site-cookbooks/odi-kibana/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: odi-kibana
+# Recipe:: default
+#
+# Copyright 2013, The Open Data Institute
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+es_boxen = search(:node, "role:elasticsearch_server AND chef_environment:#{node.chef_environment}")
+es_ips   = []
+
+es_boxen.each do |es_box|
+  es_ip = es_box["ipaddress"]
+  if es_box["rackspace"]
+    es_ip = es_box["rackspace"]["private_ip"]
+  end
+
+  es_ips << es_ip
+end
+
+node.set['kibana']['elasticsearch']['hosts'] = es_ips
+
+
+include_recipe 'kibana::default'
