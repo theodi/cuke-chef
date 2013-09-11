828213371b329d427fa06bf742888f7f64ce501e
pikesley <github@orgraphone.org>
2013-09-11 14:06:39 +0100

    Bump

--- site-cookbooks/odi-deployment/metadata.rb
+++ site-cookbooks/odi-deployment/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'Installs/Configures odi-deployment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'git'
